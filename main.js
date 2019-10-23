e8cdff9572e5cec5b4b8e3e5f8f832d26490763b
Stephanie Samson <sasamson98@hotmail.com>
2019-10-23 09:40:26 -0400

    created saveBookmark function

---
+++ main.js
@@ -0,0 +1,8 @@
+
+// Save Bookmark
+function saveBookmark() {
+    // Get site name and url
+    var siteName = document.querySelector("#siteName").value;
+    var siteURL = document.querySelector("#siteURL").value;
+}
+
